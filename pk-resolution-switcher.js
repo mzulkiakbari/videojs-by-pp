a977b1a0c2c41457f3b46f5c4e99dbddba9285a6
Muhammad Zulki Akbari <52082691+bariartz@users.noreply.github.com>
2020-07-22 03:39:38 +0700

    Update pk-resolution-switcher.js

--- pk-resolution-switcher.js
+++ pk-resolution-switcher.js
@@ -76,7 +76,8 @@
                 if (this.player_.techName_ !== 'Youtube' && this.player_.preload() === 'none' && this.player_.techName_ !== 'Flash') {
                     handleSeekEvent = 'timeupdate'
                 }
-                setSourcesSanitized(this.player_, this.src, this.options_.label, customSourcePicker).one(handleSeekEvent, function () {
+                setSourcesSanitized(this.player_, this.src, this.options_.label, customSourcePicker)
+                  .one(handleSeekEvent, function () {
                     if (this.player_.techName_ == "Flash") {
                         currentTime = 0
                     } else {
